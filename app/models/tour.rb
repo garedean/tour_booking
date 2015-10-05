f4b6aca3c6ecf6536afe3eed8a388a81b48fa97f
garedean <garrettdolson@gmail.com>
2015-10-05 12:56:05 -0700

    Add validations for booking_date form

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -6,12 +6,15 @@ class Tour < ActiveRecord::Base
             :requester_email,
             presence: true
   validates_format_of :requester_email, :with => /@/
-
   validates :requester_first_name,
             :requester_last_name,
             :requester_phone_number,
-            presence: true,
-            if: :booked_or_user_details?
+            presence: true, if: :booked_or_user_details?
+  validates :booking_date,
+            :amenities,
+            :referrer,
+            :special_requests,
+            presence: true, if: :booked_or_booking_details?
 
   def booked?
     status == 'booked'
@@ -19,5 +22,21 @@ class Tour < ActiveRecord::Base
 
   def booked_or_user_details?
     status.include?('user_details') || booked?
+  end
+
+  def booked_or_booking_details?
+    status.include?('booking_details') || booked?
+  end
+
+  def customer_full_name
+    requester_first_name + " " + requester_last_name
+  end
+
+  def amenities_list
+    amenities_list = []
+    amenities.each do |amenity|
+      amenities_list << amenity.name
+    end
+    amenities_list.join(", ")
   end
 end
